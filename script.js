f87dfd7af2e43c7951fe0849bdef71452092ee11
Aimes13 <annemariemakombe@gmail.com>
2024-07-18 14:06:42 +0200

    Update projects.html, style.css and script.js files

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const screenshotBox3 = document.getElementById('screenshot3');
 const overlay1Box3 = document.getElementById('firstlayer3');
 const overlay2Box3 = document.getElementById('secondlayer3');
 
+const screenshotBox4 = document.getElementById('screenshot4');
+const overlay1Box4 = document.getElementById('firstlayer4');
+const overlay2Box4 = document.getElementById('secondlayer4');
+
+const screenshotBox5 = document.getElementById('screenshot5');
+const overlay1Box5 = document.getElementById('firstlayer5');
+const overlay2Box5 = document.getElementById('secondlayer5');
+
+const screenshotBox6 = document.getElementById('screenshot6');
+const overlay1Box6 = document.getElementById('firstlayer6');
+const overlay2Box6 = document.getElementById('secondlayer6');
+
 //.............Project Box1
 
 function click1() {
@@ -129,4 +141,67 @@ function click3() {
 //To open website link
 overlay2Box3.addEventListener('click', () => {
     window.open("https://aimes13.github.io/Mixed-Messages-Portfolio-Project/");
+ });
+
+function mouseOut4() {
+    //To remove the project title overlay
+    overlay1Box4.style.display = 'flex';
+
+    //To display project information overlay
+    overlay2Box4.style.display = 'none';
+};
+
+function click4() {
+    //To remove the project title overlay
+    overlay1Box4.style.display = 'none';
+
+    //To display project information overlay
+    overlay2Box4.style.display = 'block';
+};
+
+//To open website link
+overlay2Box4.addEventListener('click', () => {
+    window.open("https://willcoderhz.github.io/jsfashion/about.html");
+ });
+
+function mouseOut5() {
+    //To remove the project title overlay
+    overlay1Box5.style.display = 'flex';
+
+    //To display project information overlay
+    overlay2Box5.style.display = 'none';
+};
+
+function click5() {
+    //To remove the project title overlay
+    overlay1Box5.style.display = 'none';
+
+    //To display project information overlay
+    overlay2Box5.style.display = 'block';
+};
+
+//To open website link
+overlay2Box5.addEventListener('click', () => {
+    window.open("https://appointment-planning.netlify.app/");
+ });
+
+function mouseOut6() {
+    //To remove the project title overlay
+    overlay1Box6.style.display = 'flex';
+
+    //To display project information overlay
+    overlay2Box6.style.display = 'none';
+};
+
+function click6() {
+    //To remove the project title overlay
+    overlay1Box6.style.display = 'none';
+
+    //To display project information overlay
+    overlay2Box6.style.display = 'block';
+};
+
+//To open website link
+overlay2Box6.addEventListener('click', () => {
+    window.open("https://appointment-planning.netlify.app/");
  });
